ea899d4475d9fa43e27af2de56eab45a82cd2f91
fatih <mega.wibowo21@gmail.com>
2024-08-09 18:08:17 +0700

    added get_form and fixed stock logic

--- main/admin.py
+++ main/admin.py
@@ -64,21 +64,56 @@ class BorrowedBookDetailAdmin(admin.ModelAdmin):
     exclude = [ "updated_by"]
     list_display = ["borrowed_book", "book", returned]
 
+    def delete_model(self, request, obj):
+        if obj.returned is not None:
+            obj.delete()
+
+    
+
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form =super().get_form(request, obj,  **kwargs)
+        form.base_fields["book"].queryset  = Book.objects.filter(stock__gt=0)
+        return form
+
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        book = obj.book
+        book_id = obj.book.id
+        book_object = Book.objects.get(id=book_id)
+        if obj.returned == None:
+            book_object.stock -= 1
+        else:
+            book_object.stock += 1
+        book_object.save()
+        
+    
+
         
         
 
     @admin.action(description="Mark selected as returned")
     def make_return(modeladmin, request, queryset):
+        for obj in queryset:
+            book_id = obj.book.id
+            book_object = Book.objects.get(id=book_id)
+            if book_object is None:
+                book_object.stock += 1
+            book_object.save()
         queryset.update(returned=datetime.datetime.now())
-
+        
 
 
     @admin.action(description="Mark selected as unreturned")
     def make_unreturn(modeladmin, request, queryset):
+        for obj in queryset:
+            book_id = obj.book.id
+            book_object = Book.objects.get(id=book_id)
+            if book_object is not None:
+                book_object.stock += 1
+            book_object.save()
         queryset.update(returned=None)
+
+
     actions = [make_unreturn, make_return]
 
 
